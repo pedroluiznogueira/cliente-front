df358bed8f4a28adf1d1cefcf6502af2c050e0a5
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-10 22:53:04 -0300

    Criado o component cadastro e o service cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,

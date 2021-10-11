e27d4035d3d64f15619867e1b7c3300856b68acd
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-11 02:24:23 -0300

    Criando um array do tipo Cliente, para receber cada objeto da requisição

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/model/cliente.model';
 import { CadastroService } from 'src/app/services/cadastro.service';
 
 @Component({
@@ -7,6 +8,8 @@ import { CadastroService } from 'src/app/services/cadastro.service';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+
+  private clientes: Array<Cliente> = new Array();
 
   constructor(private cadastroService: CadastroService) { }
 

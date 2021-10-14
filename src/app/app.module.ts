61379411f04921cfcb0dd879da3615dc1c945d7a
Raphael Seabra <77645109+seabraph@users.noreply.github.com>
2021-10-14 20:28:13 -0300

    alterando table servico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -44,6 +45,7 @@ import { FormularioClienteUpdateComponent } from './components/formulario-client
     MatFormFieldModule,
     HttpClientModule,
     NgbModule,
+    MatCardModule,
     MatExpansionModule,
     MatIconModule,
     MatTabsModule,

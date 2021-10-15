96ef0c2b2576ba8e5af2c9657d1a6cd481c13530
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-14 22:56:15 -0300

    Fazendo a requisição de login

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,13 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  public criarUsuario(): void {
-
+  public criarUsuario(usuario: Usuario): void {
+    this.http.post("http://localhost:4200/api/login", usuario).subscribe(resultado => console.log(resultado));
   }
 }

8ffefbdb0965c4032798d5261c44e293b0be20a8
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-15 23:13:36 -0300

    Mostrando no console o objeto contendo mensagem e status de validação da api

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
 
 @Injectable({
@@ -9,8 +10,12 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) { }
 
-  public loginUsuario(usuario: Usuario): void {
-    this.http.post("http://localhost:4200/api/login", usuario).subscribe(resultado => console.log(resultado));
+  public loginUsuario(usuario: Usuario): Observable<Object> {
+    let objeto = this.http.post<Object>("http://localhost:4200/api/login", usuario);
+    objeto.subscribe(
+      response => console.log(response)
+      )
+    return objeto;
   }
 
   public cadastroUsuario(novoUsuario: Usuario): void {

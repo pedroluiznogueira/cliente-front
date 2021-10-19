282b840d2bcd4611ef6407a01f7dcc4e31c54fa3
Raphael Seabra <77645109+seabraph@users.noreply.github.com>
2021-10-19 01:57:27 -0300

    implementando caroussel

--- src/app/components/entidades/main/main.component.ts
+++ src/app/components/entidades/main/main.component.ts
@@ -3,10 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {

c6bdb9b02beb58acc7fc21e86d79a5bf9bf04236
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-20 00:54:43 -0300

    Implementando método de pesquisa de um servico perante determinado conteúdo de título

--- src/app/services/servico.service.ts
+++ src/app/services/servico.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Servico } from '../models/servico.model';
 
 @Injectable({
@@ -8,20 +8,24 @@ import { Servico } from '../models/servico.model';
 })
 export class ServicoService {
 
+  private url: string = "https://consultoria-api.herokuapp.com";
+
+  servicoPesq: Servico = new Servico();
+
   servico: Servico = new Servico();
 
   constructor(private http: HttpClient) { }
 
   public listarServicos(): Observable<Servico[]> {
-    return this.http.get<Servico[]>("https://consultoria-api.herokuapp.com/servico");
+    return this.http.get<Servico[]>("this.url");
   }
 
   public criarServico(servico: Servico): void {
-    this.http.post("https://consultoria-api.herokuapp.com/servico", servico).subscribe(resultado => console.log(resultado));
+    this.http.post("this.url", servico).subscribe(resultado => console.log(resultado));
   }
 
   public deletarServico(id: number | undefined): void {
-    this.http.delete(`https://consultoria-api.herokuapp.com/servico/${id}`).subscribe(resultado => console.log(resultado));
+    this.http.delete(`${this.url}/servico/${id}`).subscribe(resultado => console.log(resultado));
   }
 
   public receberIdServico(id: number | undefined): void {
@@ -37,6 +41,26 @@ export class ServicoService {
   }
 
   public alterarServico(): void {
-    this.http.put("https://consultoria-api.herokuapp.com/servico", this.servico).subscribe(resultado => console.log(resultado));
+    this.http.put("this.url", this.servico).subscribe(resultado => console.log(resultado));
   }
+
+  // pesquisar servicos
+  public pesquisarServico(term: string): Observable<Servico[]> {
+    if (!term.trim()) {
+      return of([])
+    }
+
+    this.servicoPesq.titulo = term;
+
+    let obs =  this.http.post<Servico[]>("this.url", this.servicoPesq);
+
+    obs.subscribe(
+      (res) => {
+        console.log(res)
+      }                  
+    )
+
+    return obs;
+  }
+
 }

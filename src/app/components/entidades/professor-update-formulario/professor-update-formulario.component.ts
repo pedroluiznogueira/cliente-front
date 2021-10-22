017b0d1f0168fbe9c398819ae3fff9d5c4add087
Raphael Seabra <77645109+seabraph@users.noreply.github.com>
2021-10-22 03:31:16 -0300

    Corrigindo bug professor update

--- src/app/components/entidades/professor-update-formulario/professor-update-formulario.component.ts
+++ src/app/components/entidades/professor-update-formulario/professor-update-formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Professor } from 'src/app/models/professor';
+import { ProfessorService } from 'src/app/services/professor.service';
 
 @Component({
   selector: 'app-professor-update-formulario',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorUpdateFormularioComponent implements OnInit {
 
-  constructor() { }
+  public nome?: string;
+  public sobrenome?: string;
+  public email?: string;
+  public professor?: Professor;
+
+  constructor(private professorService: ProfessorService) { }
 
   ngOnInit(): void {
+  }
+
+  public envioFormulario(): void {
+    this.professor =  new Professor();
+    this.professor.nome = this.nome;
+    this.professor.sobrenome = this.sobrenome;
+    this.professor.email = this.email;
+
+    this.professorService.novoProfessor(this.professor);
+    this.nome = "";
+    this.sobrenome = "";
+    this.email = "";
   }
 
 }

b170d3bedc8013db4ced29511f40ce5423dfaf52
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-26 21:36:09 -0300

    Melhorando o layout da Wishlist e implementada a barra de pesquisa e funcionalidade de pesquisa nos cursos da wishlist

--- src/app/components/entidades/wishlist/wishlist.component.ts
+++ src/app/components/entidades/wishlist/wishlist.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Curso } from 'src/app/models/curso';
 import { Usuario } from 'src/app/models/usuario.model';
+import { CursosService } from 'src/app/services/cursos.service';
+import { WishlistService } from 'src/app/services/wishlist.service';
 import { ContaUsuarioService } from '../../conta/comp/conta-usuario.service';
 
 @Component({
@@ -11,10 +15,23 @@ import { ContaUsuarioService } from '../../conta/comp/conta-usuario.service';
 export class WishlistComponent implements OnInit {
 
   cursos: Curso[] = [];
+  sessionCursos: Curso[] = [];
+
+  pesquisando: boolean = true;
+
+  cursosFiltrados$!: Observable<Curso[]>
+  private pesquisarTerms = new Subject<string>();
 
   constructor(
+    private cursosService: CursosService,
+    private wishlistService: WishlistService,
     private contaUsuarioService: ContaUsuarioService
   ) { }
+
+  public pesquisar(term: string): void {
+    this.pesquisando = false;
+    this.pesquisarTerms.next(term);
+  }
 
   ngOnInit(): void {
     this.contaUsuarioService.emitirCursos
@@ -23,5 +40,24 @@ export class WishlistComponent implements OnInit {
           this.cursos = cursos;          
         }
       );
+
+      this.cursosFiltrados$ = this.pesquisarTerms.pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((term: string) => this.cursosService.pesquisarCursos(term)),
+      );
+  }
+
+  public sessionCurso(curso: Curso) {
+    this.sessionCursos.push(curso);
+    window.sessionStorage.setItem("cursos", JSON.stringify(this.sessionCursos));
+  }
+
+  public enviarIdCurso(curso: Curso): void {
+    this.cursosService.receberIdCurso(curso.id);
+  }
+
+  public enviarCursoId(curso: Curso){
+    this.contaUsuarioService.getCursoId(curso);
   }
 }

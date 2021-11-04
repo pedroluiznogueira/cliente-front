4bf67b6b47f3070c42c1c10e818bf8c9562c3e31
Raphael Seabra <77645109+seabraph@users.noreply.github.com>
2021-11-04 18:47:50 -0300

    implementando professor details plataforma

--- src/app/components/plataforma/professor-plataforma/professor-plataforma.component.ts
+++ src/app/components/plataforma/professor-plataforma/professor-plataforma.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Professor } from 'src/app/models/professor';
 
 @Component({
   selector: 'app-professor-plataforma',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorPlataformaComponent implements OnInit {
 
+  professor: Professor = new Professor();
+
+  urlImagem?: string = "https://udeyou.s3.sa-east-1.amazonaws.com/"
+
   constructor() { }
 
   ngOnInit(): void {
+    this.professor = JSON.parse(sessionStorage.getItem("plataforma")!)
+  }
+
+  goToLink(url: string){
+    window.open(url, "_blank");
   }
 
 }
